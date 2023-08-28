46c028fd9ef5852985206c7d23439ed682523df7
VictorL10 <mytest129867@gmail.com>
2023-08-28 14:32:23 +0300

    js proba

---
+++ JS/main.js
@@ -0,0 +1,33 @@
+
+
+//let b1 =true;
+//let b2 = folse;  
+
+
+//console.log(b1, b2);
+
+let a1 = 10;
+let a2 = 9;
+
+//console.log (a1+a2);
+
+let a3 =15;
+let a4 = 20;
+console.log (a3+a4);   
+
+let check1 = a1 > a2 && a3 < a4;
+let check2 = a1 == a2 || a3 > a4 || a1 > a3 || a4 == a2;
+let check3 = a1 > a2 && a3 <= a4 && a1 < a3 && a4 == a2;
+console.log ("check1--", check1)
+console.log ("check2--", check2)
+console.log ("check3-----", check3)
+
+if (true) {
+   console.log("Check TRUE")
+}else{
+   console.log("ELSE -- Check Else")
+} 
+
+console.log (a1+a2);   
+
+console.log ("Hello World");   
