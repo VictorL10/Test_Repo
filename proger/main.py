730d22f54ee6775d2ea7fc1d26e0a339946e0234
VictorL10 <mytest129867@gmail.com>
2023-12-12 14:11:27 +0300

    prog

---
+++ proger/main.py
@@ -0,0 +1,53 @@
+# print("Hello", 5, 6, 6, 8, sep="", end="!\n")
+# # comment
+# print("Second \" line ")
+# print("Second \" \t \\ line ")
+# print(5 // 2)
+# print( min(5, 6, 7, 3, 5))
+
+# input("ef")
+
+# number = 5
+# del number
+# # print("Result:", number)
+#
+# number = 7
+# print("Result:", number)
+#
+# digit = 4.553453
+# word = "Result:"2
+# print(word, digit)
+#
+# boolean = True
+####################################################################
+# num1 = int(input("Введите первое число: "))
+#
+# num2 = int(input("Введите второе число: "))
+#
+# num1 += 5
+#
+# print("Result:", num1 + num2)
+# print("Result:", num1 - num2)
+# print("Result:", num1 / num2)
+# print("Result:", num1 * num2)
+# print("Result:", num1 ** num2)
+# print("Result:", num1 // num2)
+#
+# word = "Hi"
+# print(word * 20)
+####################################################################
+user_data = int(input("Введите число: "))
+isHappy = False
+
+if isHappy and user_data == 6:
+    print("User is Happy")
+elif user_data == 5:
+    print("Number is 5")
+else:
+    print("User is unhappy")
+
+
+# if user_data != 5:
+#     print("Number is bigger than 5")
+#     if user_data > 6:
+#         print("True")
